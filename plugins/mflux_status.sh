ecc84f379a8a11cc4d149ba602c3bcf42a8884a9
Anthony Wu <462072+anthonywu@users.noreply.github.com>
2024-10-19 13:29:03 -0700

    improve mflux status to show output file name

--- plugins/mflux_status.sh
+++ plugins/mflux_status.sh
@@ -1,5 +1,14 @@
-if ps -ef | grep '[b]in/mflux-generate'; then
-    sketchybar --set "$NAME" icon="􀲲" label="mflux is rendering"
+#!/bin/bash
+
+ps_result=$(ps -ef | grep '[b]in/mflux-generate')
+if [ $? -eq 0 ]; then
+    output_filename=$(sed -n 's|.*--output \([^ ]*\).*|\1|p' <(echo "$ps_result"))
+    if [ -n "$output_filename" ]; then
+        label="mflux is rendering $output_filename"
+    else
+        label="mflux is rendering"
+    fi
+    sketchybar --set "$NAME" icon="􀲲" label="$label"
 else
-    sketchybar --set "$NAME" icon="" label=""
+    sketchybar --set "$NAME" icon="" label="􀜚"
 fi
